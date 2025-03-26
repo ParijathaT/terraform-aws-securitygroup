4b1131d14843234cf60ee1d1d68ed99afa3a693e
theppasiva <theppasiva@gmail.com>
2025-03-26 22:51:10 +0530

    expense infra

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "main" {
+  name        = "local.sg_final_name"
+  description = "var.sg_description"
+  vpc_id      = var.vpc_id #if you not provide vpc id it will take default vpc id
+  # inbound rules and routes should not create along with module ,it will creats user 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.common_tags,
+    var.sg_tags
+    {
+    Name = "local.sg_final_name"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "project_name"{
+
+}
+
+variable "environment"{
+
+}
+
+variable "sg_final_name"{
+
+}
+
+variable "sg_description"{
+  
+}
+
+
+variable "vpc_id"{
+
+}
+
+#optional
+variable "common_tags"{
+    type = map
+    default = {}
+}
+
+variable "sg_tags"{
+    default = {}
